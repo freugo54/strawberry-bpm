98794b900556283b5e61229ff0fd3654dcfe1ddd
freugo54 <freugo54@gmail.com>
2025-06-18 00:08:14 +0200

    Fix include, namespacing, and add dummy TrackAt

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-    Playlist window;
+    Widgets::Playlist window;
     window.resize(600, 400);
     window.show();
     return app.exec();
--- src/models/trackmodel.cpp
+++ src/models/trackmodel.cpp
@@ -1,15 +1,18 @@
-#include "trackmodel.h"
-#include "../core/track.h"
+#include "models/trackmodel.h"
+#include "core/track.h"
+
+#include <QVariant>
+#include <QString>
+#include <QModelIndex>
 
 namespace Models {
 
-TrackMetadataModel::TrackMetadataModel(QObject *parent)
-    : AbstractTrackTableModel(parent) {}
-
-TrackMetadataModel::~TrackMetadataModel() = default;
-
-int TrackMetadataModel::columnCount(const QModelIndex &) const {
-  return ColumnCount;
+// Temporäre Dummy-Funktion, um kompilierbaren Zugriff auf einen Track zu ermöglichen
+static Core::Track TrackAt(int row) {
+  Core::Track dummy;
+  dummy.SetTitle(QStringLiteral("Dummy Title"));
+  dummy.SetBpm(123.4f);
+  return dummy;
 }
 
 QVariant TrackMetadataModel::data(const QModelIndex &index, int role) const {
@@ -20,45 +23,13 @@ QVariant TrackMetadataModel::data(const QModelIndex &index, int role) const {
   const Core::Track &track = TrackAt(index.row());
 
   switch (index.column()) {
-    case Title:
+    case 0:
       return track.Title();
-    case Artist:
-      return tr("Artist");  // Platzhalter
-    case Album:
-      return tr("Album");   // Platzhalter
-    case Length:
-      return tr("3:45");    // Platzhalter
-    case Bpm:
-      return QString::number(track.Bpm(), 'f', 1);  // NEU
+    case 1:
+      return QString::number(track.Bpm(), 'f', 1);  // 1 Nachkommastelle
     default:
       return {};
   }
-}
-
-QVariant TrackMetadataModel::headerData(int section, Qt::Orientation orientation, int role) const {
-  if (role != Qt::DisplayRole || orientation != Qt::Horizontal) {
-    return {};
-  }
-
-  switch (section) {
-    case Title:
-      return tr("Title");
-    case Artist:
-      return tr("Artist");
-    case Album:
-      return tr("Album");
-    case Length:
-      return tr("Length");
-    case Bpm:
-      return tr("BPM");  // NEU
-    default:
-      return {};
-  }
-}
-
-Qt::ItemFlags TrackMetadataModel::flags(const QModelIndex &index) const {
-  if (!index.isValid()) return Qt::NoItemFlags;
-  return Qt::ItemIsSelectable | Qt::ItemIsEnabled;
 }
 
 }  // namespace Models
